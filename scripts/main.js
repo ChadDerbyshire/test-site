ef6b5ecd86ca5d1bbdc168f41680d9f37a12ef6d
Chad Derbyshire <derbyshire.chad@gmail.com>
2020-10-18 18:59:30 +0200

    Java Pop-Up Stop Touching Me

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,6 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 let myVariable;
 myVariable = 'Bob';
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
